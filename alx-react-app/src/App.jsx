abe36bedabe71fe1f605f6abf043a92525d0d96d
Esther <estherwaiganjo00@gmail.com>
2026-01-25 12:59:33 +0300

    Add Header, MainContent, and Footer components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,14 +3,14 @@
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 //import './App.css'
-import header from './Header.jsx';
+import Header from './Header';
 import MainContent from './MainContent.jsx';
 import Footer from './Footer.jsx';
 
 function App() {
   return (
     <div>
-      <header />
+      <Header />
       <MainContent />
       <Footer />
     </div>
