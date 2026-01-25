01534844d674066569ea56ebcf30f607a475f5e8
Esther <estherwaiganjo00@gmail.com>
2026-01-25 15:44:44 +0300

    Add recipe details, edit and delete functionality

---
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom';
+import { useRecipeStore } from '../store/recipeStore';
+
+const RecipeList = () => {
+  const recipes = useRecipeStore((state) => state.recipes);
+
+  return (
+    <div>
+      <h2>Recipes</h2>
+
+      {recipes.map((recipe) => (
+        <div key={recipe.id} style={{ marginBottom: '10px' }}>
+          <Link to={`/recipe/${recipe.id}`}>
+            <h3>{recipe.title}</h3>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default RecipeList;
