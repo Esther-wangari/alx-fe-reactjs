7bcdb722979957d95478a2b01185d03f237a95ae
Esther <estherwaiganjo00@gmail.com>
2026-01-25 13:20:16 +0300

    Add UserProfile component and use it in App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,18 +3,19 @@
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 //import './App.css'
-import Header from './Header';
-import MainContent from './MainContent.jsx';
-import Footer from './Footer.jsx';
-
+//import Header from './Header';
+//import MainContent from './MainContent.jsx';
+//import Footer from './Footer.jsx';
+import UserProfile from './components/UserProfile';
 function App() {
   return (
     <div>
-      <Header />
-      <MainContent />
-      <Footer />
+      <UserProfile 
+        name="Alice" 
+        age="25" 
+        bio="Loves hiking and photography" 
+      />
     </div>
   );
 }
-
 export default App;
